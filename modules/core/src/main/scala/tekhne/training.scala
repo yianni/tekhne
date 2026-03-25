36dbeae70621b92a3afcf24279d464f7cf7f7dec
John Gerassimou <john.gerassimou@gmail.com>
2026-03-25 15:40:48 -0400

    feat: add dataset shuffling to training

--- modules/core/src/main/scala/tekhne/training.scala
+++ modules/core/src/main/scala/tekhne/training.scala
@@ -2,7 +2,20 @@ package tekhne
 
 import tekhne.Linalg._
 
+import scala.util.Random
+
 object Training:
+  private def trainDeterministic(
+      network: Network,
+      data: Vector[(Vec, Vec)],
+      config: TrainingConfig
+  ): Network =
+    Iterator
+      .fill(config.epochs)(())
+      .foldLeft(network) { case (current, _) =>
+        trainEpoch(current, data, config.learningRate)
+      }
+
   def step(
       network: Network,
       input: Vec,
@@ -38,16 +51,27 @@ object Training:
       config: TrainingConfig
   ): Network =
     require(
-      config.learningRate > 0.0,
-      s"learning rate must be positive, got ${config.learningRate}"
+      !config.shuffleEachEpoch,
+      "shuffleEachEpoch = true requires the Training.train overload that accepts a Random"
     )
-    require(config.epochs > 0, s"epochs must be positive, got ${config.epochs}")
+    require(data.nonEmpty, "training data must be non-empty")
+    trainDeterministic(network, data, config)
+
+  def train(
+      network: Network,
+      data: Vector[(Vec, Vec)],
+      config: TrainingConfig,
+      rng: Random
+  ): Network =
     require(data.nonEmpty, "training data must be non-empty")
 
     Iterator
       .fill(config.epochs)(())
       .foldLeft(network) { case (current, _) =>
-        trainEpoch(current, data, config.learningRate)
+        val epochData =
+          if config.shuffleEachEpoch then rng.shuffle(data)
+          else data
+        trainEpoch(current, epochData, config.learningRate)
       }
 
   def datasetLoss(network: Network, data: Vector[(Vec, Vec)]): Double =
